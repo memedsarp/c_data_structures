15f7ed1a5672f81ad3b351e84330bed52eea5754
memedsarp <sarpsolakoglu@gmail.com>
2024-06-23 19:00:26 +0300

    day 3

--- linked_list.c
+++ linked_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct Node
 {
@@ -11,12 +12,16 @@ struct Node* head;
 
 void Insert(int x);
 void Insertn(int x, int n);
+void Deleten(int n);
+void Reverse();
 void Print();
 
 int main(int argc, char const *argv[])
 {
   head = NULL;
 
+  // Manuel entry
+  /*
   int n, x;
   printf("How many numbers do you want to insert? ");
   scanf("%d", &n);
@@ -27,13 +32,28 @@ int main(int argc, char const *argv[])
     scanf("%d", &x);
     Insert(x);
   }
+  */
+  //Automated entry
 
-  Insertn(100, 0);
-  Insertn(200, 1);
-  Insertn(300, 60);
+  srand(time(NULL));
+
+  for(int i=0; i<1; i++)
+  {
+    Insert(rand() % 100);
+  }
+
+  Insert(100);
+  Insertn(200, 0);
+  Insertn(300, 0);
+  Insertn(800, 85);
+
+  Deleten(85);
+  Deleten(0);
 
   Print();
-  printf("%d", head->data);
+  
+  Reverse();
+  Print();
   return 0;
 }
 
@@ -58,19 +78,19 @@ void Insert(int x)
 
 void Insertn(int x, int n)
 {
-  struct Node* newNode = malloc(sizeof(struct Node));
-  newNode->data = x;
-  newNode->next = NULL;
+  struct Node* temp = malloc(sizeof(struct Node));
+  temp->data = x;
+  temp->next = NULL;
 
-  struct Node* temp = head;
+  struct Node* tail = head;
   for(int i=0; i<n-1; i++)
   {
-    if (temp->next == NULL)
+    if (tail->next == NULL)
     {
       break;
     } else
     {
-      temp = temp->next;
+      tail = tail->next;
     }
   }
   if (head == NULL)
@@ -78,22 +98,88 @@ void Insertn(int x, int n)
     Insert(x);
   } else if (n == 0)
   {
-    newNode->next = head;
-    head = newNode;
-  } else if (temp->next == NULL)
+    temp->next = head;
+    head = temp;
+  } else if (tail->next == NULL)
   {
-    temp->next = newNode;
+    tail->next = temp;
   } else
   {
-    newNode->next = temp->next;
-    temp->next = newNode;
+    temp->next = tail->next;
+    tail->next = temp;
+  }
+}
+
+void Deleten(int n)
+{
+  if (n == 0)
+  {
+    struct Node* temp = head;
+    head = temp->next;
+    temp->next = NULL;
+    free(temp);
+    return;
+  } 
+  struct Node* tail = head;
+
+  for(int i=1; i<n; i++)
+  {
+    if (tail->next == NULL)
+    {
+      return;
+    }
+    tail = tail->next;
+  }
+
+  if (tail->next == NULL)
+  {
+    return;
+  }
+  {
+    struct Node* temp = tail->next;
+    tail->next = tail->next->next;
+    temp->next = NULL;
+    free(temp);
+  }
+}
+
+void Reverse()
+{
+  if (head->next != NULL)
+  {
+  struct Node* temp1 = head;
+  struct Node* temp2 = head->next;
+  struct Node* temp3 = head->next->next;
+
+  temp1->next = NULL;
+  while(temp3 != NULL)
+  {    
+    temp2->next = temp1;
+    temp1 = temp2;
+    temp2 = temp3;
+    temp3 = temp3->next;
+  }
+
+  temp2->next = temp1;
+  head = temp2;
+  } else if (head->next == NULL)
+  {
+    return;
+  } else if (head->next->next == NULL)
+  {
+    struct Node* temp1 = head;
+    struct Node* temp2 = head->next;
+
+    temp2->next = temp1;
+    temp1->next = NULL;
+    head = temp2;
   }
 }
 
 void Print()
 {
   struct Node* temp = head;
-  printf("The list is: ");
+  printf("\nThe list is: ");
   while(temp != NULL)
   {
     printf(" %d", temp->data);
